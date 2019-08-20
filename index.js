3e346f73ee7cad1c2a848730fedab4a13f9eb65b
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-20 22:13:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var kittens = [] //define your array here
 var kittens = ["Milo", "Otis", "Garfield"];
-
+function destructivelyAppendKitten(name){
+    return kittens.push(name);
 // Add your functions and code here
+}
